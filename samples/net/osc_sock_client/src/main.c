0521d44f62c8b82f852835b5261472dfe97763be
Eve Redero <eve.redero@gmail.com>
2024-05-20 18:01:12 +0200

    test osc in ethernet

---
+++ samples/net/osc_sock_client/src/main.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023 Craig Peacock.
+ * Copyright (c) 2017 ARM Ltd.
+ * Copyright (c) 2016 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/net_event.h>
+#include <errno.h>
+#include "echo-client.h"
+
+void main(void)
+{
+	echo_client();
+}
