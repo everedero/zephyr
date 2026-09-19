2c9e68fc44ed6d269b8988e1f04122905bfd97de
Eve Redero <eve.redero@gmail.com>
2026-09-19 14:24:28 +0200

    [DTS] Update FAHV3

--- boards/st/fahv3/board.cmake
+++ boards/st/fahv3/board.cmake
@@ -2,7 +2,7 @@
 
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
-board_runner_args(jlink "--device=STM32F722ZE" "--speed=4000")
+board_runner_args(jlink "--device=STM32F730Z8" "--speed=4000")
 
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
